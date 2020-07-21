6fc9ec35f9efcb557d757ee1d7ca7ffab5a1ad00
somikdhar9897 <45973106+somikdhar9897@users.noreply.github.com>
2020-07-21 16:41:11 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-C6WzUI26ArFRBRhzgRHTSdLfMqigl7x'
-export_file_name = 'mask'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-MTsJtS-g_96u3jbdRLFt2vC0oS755Vv'
+export_file_name = 'mask.pkl'
 
 classes = ["Mask","No_Mask"]
 path = Path(__file__).parent
